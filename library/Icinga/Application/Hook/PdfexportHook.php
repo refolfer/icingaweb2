4540291510e09a583a359c688f9a57c948f906ff
Alexander Rieß <alexander.riess@icinga.com>
2026-04-10 13:03:41 +0200

    Remove types to be backward compatible with older pdfexport versions

--- library/Icinga/Application/Hook/PdfexportHook.php
+++ library/Icinga/Application/Hook/PdfexportHook.php
@@ -5,7 +5,9 @@
 
 namespace Icinga\Application\Hook;
 
+use Icinga\Application\Hook;
 use ipl\Html\ValidHtml;
+use RuntimeException;
 
 /**
  * Base class for the PDF Export Hook
@@ -13,19 +15,37 @@ use ipl\Html\ValidHtml;
 abstract class PdfexportHook
 {
     /**
+     * Get the first hook
+     *
+     * @return static
+     */
+    public static function first()
+    {
+        if (! Hook::has('Pdfexport')) {
+            throw new RuntimeException('No PDF exporter available');
+        }
+        $pdfexport = Hook::first('Pdfexport');
+        if (! $pdfexport->isSupported()) {
+            throw new RuntimeException('PDF exporter is not supported');
+        }
+        return $pdfexport;
+    }
+
+    /**
      * Get whether PDF export is supported
      *
      * @return  bool
      */
-    abstract public function isSupported(): bool;
+    abstract public function isSupported();
 
     /**
      * Render the specified HTML to PDF and stream it to the client
      *
      * @param ValidHtml $html The HTML to render to PDF
      * @param string $filename The filename for the generated PDF
+     * @return never
      */
-    abstract public function streamPdfFromHtml(ValidHtml $html, string $filename): never;
+    abstract public function streamPdfFromHtml($html, $filename);
 
     /**
      * Render the specified HTML to PDF and return the PDF document as a string
@@ -34,5 +54,5 @@ abstract class PdfexportHook
      *
      * @return string
      */
-    abstract public function htmlToPdf(ValidHtml $html): string;
+    abstract public function htmlToPdf($html);
 }
